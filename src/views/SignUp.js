ec1245eee9579cb249e22a183f284d3c89c7bad8
stalliardo <46523228+stalliardo@users.noreply.github.com>
2021-02-15 17:47:12 +0000

    Feature/finish the sign up form (#6)
    * Added the error text to the form
    
    * Added neon glow fonts and added as the title on the sign up form
    
    Co-authored-by: stalliardo187 <darrenstallard27@hotmail.com>

--- src/views/SignUp.js
+++ src/views/SignUp.js
@@ -6,6 +6,7 @@ import axios from 'axios';
 import { validEmail } from '../util/validators.js';
 import "../styles/views/input.scss";
 import "../styles/views/form.scss";
+import "../styles/views/fonts.scss";
 import Button from '../components/Button';
 
 class SignUp extends Component {
@@ -20,9 +21,10 @@ class SignUp extends Component {
             confirmPassword: "",
             formIsValid: false,
             isSaving: false,
-            submitButtonDisabled: true
+            submitButtonDisabled: true,
+            errorText: ""
         }
-    }
+    };
 
     setSubmitButtonDisabledState = () => {
         const isEmailValid = validEmail(this.state.email);
@@ -36,13 +38,26 @@ class SignUp extends Component {
         this.setState({
             submitButtonDisabled: !formIsValid
         });
-    }
+    };
 
     handleChange = (event) => {
         this.setState({
             [event.target.name]: event.target.value
         }, () => {
             this.setSubmitButtonDisabledState()
+        });
+    };
+
+    errorTextHandler = (error) => {
+        let errorMessage = "";
+
+        if(error.message) {
+           errorMessage = error.message
+        } else {
+            errorMessage = "Something went wrong! Please try again."
+        }
+        this.setState({
+            errorText: errorMessage
         });
     };
 
@@ -67,7 +82,7 @@ class SignUp extends Component {
             this.props.history.push("/")
         }).catch((error) => {
             console.log("an error occured while creating the new user. Error = ", error);
-            // TODO -> Display errors on page/form
+            this.errorTextHandler(error);
         }).finally(() => {
             this.setState({
                 isSaving: false
@@ -80,7 +95,7 @@ class SignUp extends Component {
     }
 
     OnSigninClicked = () => {
-        this.props.history.push("/login")
+        this.props.history.push("/login");
     }
 
     onGoogleSignup = () => {
@@ -100,7 +115,7 @@ class SignUp extends Component {
             if (error.response.data.error === "Account already exists") {
                 this.props.history.push("/");
             } else {
-                // TODO -> Display errors on page/form
+                this.errorTextHandler(error);
             }
         });
     }
@@ -108,7 +123,8 @@ class SignUp extends Component {
     render() {
         return (
             <div className="sign-up-form">
-                <h1>Sign up TODO Add glow effect to titles</h1>
+                {/* <h1>Sign up TODO Add glow effect to titles</h1> */}
+                <h2 className="title">Sign up</h2>
                 <p>Or <span onClick={this.OnSigninClicked}>Sign in to your account</span></p>
                 <div className="neon-borders">
                     <form onSubmit={(e) => this.onSubmit(e)}>
@@ -117,11 +133,10 @@ class SignUp extends Component {
                         <input className="e-input" type="email" name="email" placeholder="Email Address" onChange={this.handleChange} />
                         <input className="e-input" type="password" name="password" placeholder="Password" onChange={this.handleChange} />
                         <input className="e-input" type="password" name="confirmPassword" placeholder="Confirm Password" onChange={this.handleChange} />
-
+                        {this.state.errorText ? <p className="error-text">{this.state.errorText}</p> : null}
                         <div className="signup-form-button-aligner">
                             <Button text="Submit" type="submit" isLoading={this.state.isSaving} disabled={this.state.submitButtonDisabled} />
                         </div>
-
                     </form>
                     <div className="signup-form-button-aligner">
                         <Button clickHandler={this.onGoogleSignup} text="Sign up with Google" />
