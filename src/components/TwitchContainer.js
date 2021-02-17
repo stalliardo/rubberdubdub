a2f936451fcd9d4560901154d36e7eb4f017deae
stalliardo <46523228+stalliardo@users.noreply.github.com>
2021-02-17 14:07:17 +0000

    Added todos to the twitch.scss file (#9)
    * Added todos to the twitch.scss file
    
    * Added the react-player and added video to the homepage
    
    * Added imgaes to scrollable container and set opacity to highlight the video
    
    * Changed the max-width of the outlet container
    
    Co-authored-by: stalliardo187 <darrenstallard27@hotmail.com>

--- src/components/TwitchContainer.js
+++ src/components/TwitchContainer.js
@@ -1,25 +1,79 @@
 import React, { Component } from 'react';
 import '../styles/components/twitch-container.scss';
 // import Spinner from './Spinner';
+import ReactPlayer from "react-player"
+
 
 class TwitchContainer extends Component {
     constructor(props) {
         super(props);
     }
 
-    render() {
+    componentDidMount() {
+        var video = document.querySelector('video');
+        console.log("video = ", video);
+    }
 
+
+    videoStarted = () => {
+        console.log("Video started called");
+    }
+
+    render() {
+        const videoUrl = 916261897; // TODO -> This will need to be set when the user clicks on one of the thunbnails or set by default
         return (
             <div className="twitch-container">
-                <p>Twitch container</p>
                 <div className="twitch-scroller">
-                    <p>Twitch scroller</p>
+                    <div className="twitch-video queued small">
+                        <img src="https://static-cdn.jtvnw.net/cf_vods/d3c27h4odz752x/e2b4c0f60e7cc27046d1_danrob911_41124685996_1613477032//thumb/thumb0-160x90.jpg" />
+                    </div>
+                    <div className="twitch-video queued medium">
+                        <img src="https://static-cdn.jtvnw.net/cf_vods/d2nvs31859zcd8/6b7e0ce597266c0f0744_danrob911_40531102477_1612985446//thumb/thumb0-240x135.jpg" />
+                    </div>
+                    <div className="twitch-video playing">
+                        <ReactPlayer
+                            url={`https://www.twitch.tv/videos/${videoUrl}`}
+                            onPlay={this.videoStarted}
+                            width="480px"
+                            height="270px"
+                            muted={false}
+                            playing={false}
+                            controls={true}
+                            style={{
+                                // backgroundColor: "red"
+                            }}
+                            config={{
+                                twitch: {
+                                    options: {
+                                        allowfullscreen: true
+                                    }
+                                }
+                            }}
+                        />
+                    </div>
+                    <div className="twitch-video queued medium">
+                        <img src="https://static-cdn.jtvnw.net/cf_vods/d3c27h4odz752x/90c4ebf45046a4d16c4a_danrob911_41043544780_1612817006//thumb/thumb0-240x135.jpg" />
+                    </div>
+                    <div className="twitch-video queued small">
+                        <img src="https://static-cdn.jtvnw.net/cf_vods/d3c27h4odz752x/88a70d5529780a0c17ae_danrob911_40989853612_1612467080//thumb/thumb0-160x90.jpg" />
+                    </div>
                 </div>
-                <div className="twitch-thumbnails">
-                    <p>Twitch thumbs</p>
+                <div className="twitch-thumbnail-container">
+                    <div className="twitch-thumbnail">
+                        {/* <img src="https://static-cdn.jtvnw.net/cf_vods/d3c27h4odz752x/e2b4c0f60e7cc27046d1_danrob911_41124685996_1613477032//thumb/thumb0-200x200.jpg" /> */}
+                    </div>
                 </div>
             </div>
         )
     }
 }
 export default TwitchContainer;
+
+
+// TODO -> Remove...
+// 3m0xbu8of3bx3bvh4jes85a06tfrwt
+
+{/* <img src="https://static-cdn.jtvnw.net/cf_vods/d3c27h4odz752x/e2b4c0f60e7cc27046d1_danrob911_41124685996_1613477032//thumb/thumb0-200x200.jpg"/> */ }
+// The above image source is hard coded and will need to be obtained in an other way
+
+// App access tokens are meant only for server-to-server API requests and should never be included in client code.
