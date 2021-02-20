b19bb7a50c2428fbf2be73b90fe4c178ea14c53e
stalliardo187 <darrenstallard27@hotmail.com>
2021-02-20 17:41:46 +0000

    Added dimming logic to the images and indicators

--- src/components/Clips.js
+++ src/components/Clips.js
@@ -12,8 +12,12 @@ class Clips extends Component {
 
         this.state = {
             isLoading: false,
+            prompt: "",
             clipData: [],
-            saveButtonDisabled: false
+            saveButtonDisabled: false,
+            isSettingScrollerClips: false,
+            isAddingClip: false,
+            isRemovingClip: false,
         }
     }
 
@@ -38,12 +42,136 @@ class Clips extends Component {
         // TODO -> Will be based off the selected images count
     }
 
+    onSetIsScrollerClip = (clip) => {
+        console.log("some func called + e = ", clip);
+        const isScrollerClip = clip.isScrollerClip;
+        const isAssigning = !isScrollerClip;
+
+        if (isAssigning) {
+
+            const newClipArray = this.state.clipData.map((clip) => {
+                if (!clip.isScrollerClip) {
+                    clip.imgClass = "dim"
+                    return clip
+                } else return clip
+            })
+
+            console.log("newCliparray = ", newClipArray);
+
+            this.setState({
+                clipData: newClipArray,
+                prompt: "Select the clip to replace"
+            })
+
+
+
+        } else {
+
+            const newClipArray = this.state.clipData.map((clip) => {
+                if (clip.isScrollerClip || clip.isMainScrollerClip) {
+                    clip.imgClass = "dim"
+                    return clip
+                } else return clip
+            })
+
+            console.log("newClip array = ", newClipArray);
+
+
+            this.setState({
+                clipData: newClipArray,
+                prompt: "Select the clip to replace"
+            })
+        }
+
+        // Will need to check what operation is happening ie adding a clip or removing, do this via isScollerClip true or false
+        // if scroller clip t
+
+        this.setState({
+            isSettingScrollerClips: true
+        })
+    };
+
+    onReplaceClip = (clip) => {
+        console.log("on replace called + clip = ", clip);
+    }
+
+    onCancelSettingScrollerClips = () => {
+        
+        this.setState({
+            isSettingScrollerClips: false,
+            clipData: this.state.clipData.map((clip) => {
+                clip.imgClass = "";
+                return clip;
+            }),
+            prompt: ""
+        })
+    }
+
+    imgStyler = () => {
+        return "SOMECLASS"
+    }
+
     render() {
 
         return (
             <div className="clips-page">
-                {!this.state.isLoading ?  <ClipActions saveButtonDisabled={this.state.saveButtonDisabled}/> : null}
-                {this.state.isLoading ? <Spinner height="100px" width="100px" marginTop="200px" /> : <ClipDisplay clipData={this.state.clipData} />}
+                {!this.state.isLoading ?
+                    // <ClipActions saveButtonDisabled={this.state.saveButtonDisabled} isSettingScrollerClips={this.state.isSettingScrollerClips}/> 
+                    <div className="clip-actions">
+                        <p>
+                            {this.state.prompt}
+                        </p>
+                        <div className="clip-buttons">
+
+                            <Button text="Save" disabled={this.state.saveButtonDisabled} />
+                            {this.state.isSettingScrollerClips ? <Button text="Cancel" clickHandler={this.onCancelSettingScrollerClips} /> : null}
+                        </div>
+                    </div>
+                    : null}
+
+                {this.state.isLoading ?
+                    <Spinner height="100px" width="100px" marginTop="200px" /> :
+                    <div className="clip-images">
+                        {this.state.clipData.map((clip, index) => {
+                            return (
+                                <div key={index} className="clip-card">
+                                    {/* {TODO -> Only display the indicator if its in the scroller} */}
+                                    {clip.isScrollerClip && !clip.isMainScrollerClip ? <div className={`clip-card-indicator ${clip.imgClass}`}></div> : null}
+                                    {clip.isMainScrollerClip ? <div className={`clip-card-indicator main ${clip.imgClass}`}></div> : null}
+                                    {clip.imgClass !== "dim" ?
+                                        <div className="hidden-clip-options">
+
+
+
+
+
+                                            {
+                                                this.state.isSettingScrollerClips ?
+                                                   <p onClick={this.onReplaceClip.bind(this, clip)}>Replace with this clip</p>
+
+                                                    :<div>
+                                                        <p onClick={this.onSetIsScrollerClip.bind(this, clip)}>{clip.isScrollerClip ? "Remove from scroller" : "Add to scroller"}</p>
+                                                        <p>{clip.isMainScrollerClip ? "Unmark as main clip" : "Set as main clip in scroller"}</p>
+                                                    </div>
+                                            }
+
+
+
+
+
+
+
+
+
+                                        </div>
+                                        : null}
+                                    <img src={clip.thumbnailUrl} className={clip.imgClass} />
+                                    <p>{clip.clipTitle}</p>
+                                </div>
+                            )
+                        })}
+                    </div>
+                }
             </div>
         )
     }
@@ -52,51 +180,7 @@ class Clips extends Component {
 export default Clips;
 
 
-function ClipDisplay(props) {
-
-    // function showClipOptions(e){
-    //     console.log("Show clip ops called + e = ", e);
-    // }
-
-    function someFunction(e){
-        console.log("some func called + e = ", e);
-    }
-
-
-    return (
-        <div className="clip-images">
-            {props.clipData.map((clip, index) => {
-                return (
-                    <div key={index} className="clip-card">
-                        {/* {TODO -> Only display the indicator if its in the scroller} */}
-                        {clip.isScrollerClip && !clip.isMainScrollerClip ? <div className="clip-card-indicator"></div> : null}
-                        {clip.isMainScrollerClip ? <div className="clip-card-indicator main"></div> : null}
-                        <div className="hidden-clip-options">
-                            <p onClick={someFunction.bind(this, clip)}>{clip.isScrollerClip ? "Remove from scroller" : "Add to scroller"}</p>
-                            <p>{clip.isMainScrollerClip ? "Unmark as main clip" : "Set as main clip in scroller"}</p>
-                        </div>
-                        <img src={clip.thumbnailUrl} />
-                        <p>{clip.clipTitle}</p>
-                    </div>
-                )
-            })}
-        </div>
-    )
-}
-
-function ClipActions(props) {
-    return (
-      <div className="clip-actions">
-          <p>
-              Select the five clips that are to be displayed in the homepage scroller.
-          </p>
-         <div className="clips-button">
-            <Button text="Save" disabled={props.saveButtonDisabled}/>
-         </div>
-      </div>
-    )
-}
-
-// How will i determine if the clip is in the scroller???
-// Will also need to get the five scroller clips from the backend then check the id (url) aginst those returned from the GET /clips call
-// Need an admin doc in the cloud DB 
+// The pop up options needs to be smarted
+// When the user is either adding or deleting the content should change
+// if adding the box should now say "remove this clip from scroller"
+// If the user is removing the box should say "Replace with this clip"
