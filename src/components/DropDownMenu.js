305fca1b9e2fded9fdf0a141ca82b476fda9d45c
stalliardo187 <darrenstallard27@hotmail.com>
2021-03-10 09:31:03 +0000

    Added the spinner to the dropdown menu comp

--- src/components/DropDownMenu.js
+++ src/components/DropDownMenu.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import '../styles/components/drop-down-menu.scss';
 import DropDownItem from './DropDownItem';
+import Spinner from './Spinner';
 
 class DropDownMenu extends Component {
     constructor(props) {
@@ -9,10 +10,14 @@ class DropDownMenu extends Component {
 
     render() {
         return (
-            <div className={`drop-down-menu-container ${this.props.data.length ? "open" : "closed"}`}>
-                 {
+            <div className={`drop-down-menu-container ${this.props.data.length ? "open" : "closed"} ${this.props.isLoading ? "is-loading" : null}`}>
+                {
+                    this.props.isLoading ? <Spinner height="20px" width="20px" /> : null
+                }
+
+                {
                     this.props.data.map((item, index) => {
-                        return <DropDownItem key={index} data={item} textKey={this.props.textKey}/>
+                        return <DropDownItem key={index} data={item} textKey={this.props.textKey} />
                     })
                 }
             </div>
--- src/components/squad/CreateSquadForm.js
+++ src/components/squad/CreateSquadForm.js
@@ -4,6 +4,7 @@ import Button from '../Button';
 import DropDownMenu from '../DropDownMenu';
 import SelectMenu from '../SelectMenu';
 import _ from 'lodash';
+import Spinner from '../Spinner';
 
 class CreateSquadForm extends Component {
 
@@ -11,7 +12,8 @@ class CreateSquadForm extends Component {
         super(props);
 
         this.state = {
-            soldierData: []
+            soldierData: [],
+            isSearching: false,
         }
 
         this.startSearch = _.debounce(this.startSearch, 2000);
@@ -21,8 +23,10 @@ class CreateSquadForm extends Component {
         const searchTerm = e.target.value;
 
         if(searchTerm.length > 2) {
-            console.log("searching....");
 
+            this.setState({
+                isSearching: true
+            })
             // This will then call the backend api "searchSoldiers/searchterm"
             // When a response is returned it will load that data into the dropdownmenu
             axios.get(`/searchSoldiers/${searchTerm}`).then((data) => {
@@ -32,10 +36,15 @@ class CreateSquadForm extends Component {
                 })
             }).catch((error) => {
                 console.log("error getting soldier data. Error: ", error);
+            }).finally(() => {
+                this.setState({
+                    isSearching: false
+                })
             })
         } else if(searchTerm.length < 3) {
             this.setState({
-                soldierData: []
+                soldierData: [],
+                isSearching: false
             })
         }
     }
@@ -54,8 +63,12 @@ class CreateSquadForm extends Component {
                 <h1>Create Squad</h1>
                 <div className="neon-borders">
                 <input className="e-input" type="text" name="squadName" placeholder="Squad Name" onChange={this.handleChange}/>
-                <input className="e-input" type="text" name="soldierSearch" placeholder="Search for soldiers (case sensitive)" onChange={this.onSearchForSoldiers}/>
-                <DropDownMenu data={this.state.soldierData} textKey="activisionAccount"/>
+                <input className="e-input with-spinner" type="text" name="soldierSearch" placeholder="Search for soldiers (case sensitive)" onChange={this.onSearchForSoldiers}/>
+                <DropDownMenu 
+                    data={this.state.soldierData} 
+                    textKey="activisionAccount"
+                    isLoading={this.state.isSearching}
+                />
                 </div>
             </div>
         )
