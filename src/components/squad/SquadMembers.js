665c77558651e2db455d93abccaff894f1bfd3c1
stalliardo187 <darrenstallard27@hotmail.com>
2021-04-04 10:56:47 +0100

    WIP

--- src/components/squad/SquadMembers.js
+++ src/components/squad/SquadMembers.js
@@ -5,12 +5,12 @@ import SquadMembersTable from './SquadMembersTable';
 
 class SquadMembers extends Component {
 
-    constructor(props){
+    constructor(props) {
         super(props);
 
     }
 
-    render(){
+    render() {
         console.log("squad Data from memeber = ", this.props.squadData);
 
         return (
@@ -18,15 +18,17 @@ class SquadMembers extends Component {
                 <h1 className="neon-text small">Squad Members ({this.props.squadData.members.length})</h1>
                 <h2>Squad General: <span>{this.props.squadData.general}</span></h2>
                 <div className="squad-members-table-container">
-                    <SquadMembersTable data={this.props.squadData} isGeneral={this.props.isGeneral}/>
+                    <SquadMembersTable data={this.props.squadData} isGeneral={this.props.isGeneral} />
                 </div>
-               {this.props.isGeneral ? 
-                <div className="squad-members-add-button">
-                <Button text="Add Members"/>
-            </div> : null}
+                {this.props.isGeneral ?
+                    <div className="squad-members-add-button">
+                        <Button text="Add Members" />
+                    </div>
+                    : null
+                }
             </div>
         )
-    }    
+    }
 }
 
 export default SquadMembers;
